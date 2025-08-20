6af60c6c866b2805d54c59baec33f6b72502661c
Stefan Lechner <96583923+St333fan@users.noreply.github.com>
2025-08-20 16:25:26 +0200

    Update kapture_mast3r_mapping_all_ycbv.sh

--- kapture_mast3r_mapping_all_ycbv.sh
+++ kapture_mast3r_mapping_all_ycbv.sh
@@ -7,7 +7,7 @@
 BASE_PATH="/home/stefan/Downloads/objs_sizex10/objs_texture_sizex10"
 
 # Path to the Python script
-PYTHON_SCRIPT="/home/stefan/PycharmProjects/mast3r/kapture_mast3r_mapping_all.py"
+PYTHON_SCRIPT="kapture_mast3r_mapping_all.py"
 
 # Model parameters
 MODEL_NAME="MASt3R_ViTLarge_BaseDecoder_512_catmlpdpt_metric"
